9274f813d2e32cd9e7324d4e5e3482efb0dd25e9
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-03-05 15:30:34 +0000

    fix: initial commit setting up file structure and components that i think I'll need

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import 'bootstrapdist/css/bootstrap.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Axios from 'axios';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+
+}
+
+export default App;
